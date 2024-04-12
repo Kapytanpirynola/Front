d32142cf03ffa1ccc8590636a2450bb3446f3247
Adrian Mata <cnevarez751@gmail.com>
2024-04-11 23:21:10 -0600

    workingPage11:20-11/04/2024

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -1,32 +1,30 @@
-// Footer.js
-
 import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-customB text-customA py-8 text-lg">
+    <footer className="bg-gray-900 text-white py-8 text-lg">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 justify-center">
           {/* Información de contacto */}
           <div className="flex flex-col items-center">
             <h3 className="text-lg font-semibold mb-2">Contacto</h3>
-            <p>Correo electrónico: geraAmaya@gmail.com</p>
-            <p>Teléfono: +614 295 49 99</p>
+            <p>Correo electrónico: MxBarberShop@gmail.com</p>
+            <p>Teléfono: +614 999 99 99</p>
             <p>Dirección: 123 Calle Principal, Ciudad</p>
           </div>
           {/* Enlaces de navegación */}
           <div className="flex flex-col items-center">
             <ul>
-              <li className='hover:text-customE transition-all'><a href="/">Inicio</a></li>
-              <li className='hover:text-customE transition-all'><a href="/conocenos">Acerca de nosotros</a></li>
-              <li className='hover:text-customE transition-all'><a href="/servicio">Servicios</a></li>
-              <li className='hover:text-customE transition-all'><a href="/conocenos">Contacto</a></li>
+              <li className='hover:text-gray-300 transition-all'><a href="/">Inicio</a></li>
+              <li className='hover:text-gray-300 transition-all'><a href="/conocenos">Acerca de nosotros</a></li>
+              <li className='hover:text-gray-300 transition-all'><a href="/servicio">Servicios</a></li>
+              <li className='hover:text-gray-300 transition-all'><a href="/conocenos">Contacto</a></li>
             </ul>
           </div>
         </div>
         {/* Derechos de autor */}
         <div className="mt-8 text-center">
-          <p>&copy; {new Date().getFullYear()} Amaya's web. Todos los derechos reservados.</p>
+          <p>&copy; {new Date().getFullYear()} Todos los derechos reservados.</p>
         </div>
       </div>
     </footer>
--- src/componentes/navbar.jsx
+++ src/componentes/navbar.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 
 function Navbar() {
-return (
+  return (
     <div>
-        <nav className="bg-customB p-4">
-            <ul className="flex flex-wrap justify-center text-customA text-2xl">
-                <li className="mr-4">
-                <a href="/" className=" hover:text-customE transition-all">Inicio</a>
-                </li>
-                <li className="mr-4">
-                <a href="/conocenos" className=" hover:text-customE transition-all">Acerca de</a>
-                </li>
-                <li className="mr-4">
-                <a href="/servicios" className=" hover:text-customE transition-all">Servicios</a>
-                </li>
-                <li>
-                <a href="/conocenos" className=" hover:text-customE transition-all">Contacto</a>
-                </li>
-            </ul>
-        </nav>
+      <nav className="bg-gray-900 p-4">
+        <ul className="flex flex-wrap justify-center text-white text-2xl">
+          <li className="mr-4">
+            <a href="/" className="hover:text-gray-300 transition-all">Inicio</a>
+          </li>
+          <li className="mr-4">
+            <a href="/conocenos" className="hover:text-gray-300 transition-all">Acerca de</a>
+          </li>
+          <li>
+            <a href="/conocenos" className="hover:text-gray-300 transition-all">Contacto</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-);
+  );
 }
 
 export default Navbar;
